8559e22f4f6d00d10dc1658ef22699cb9acc30b3
alexarirok <akorir233@gmail.com>
2019-05-20 10:57:34 +0300

    Writting functions in python

---
+++ fun1.py
@@ -0,0 +1,18 @@
+def printname():
+    print("Korir")
+
+
+
+def swahilicode():
+    #swahili=input("We teach python programming. ")
+    print("We teach python programming. ")
+
+
+def letters_of_my_name():
+    name = str(input("What is your name? "))
+    for i in name:
+        print(i)
+
+letters_of_my_name()
+swahilicode()
+printname()
