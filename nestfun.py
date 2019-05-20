ed62d4ce140f50dfd7da30492179ef09ff222641
alexarirok <akorir233@gmail.com>
2019-05-20 11:44:25 +0300

    nesting functions

--- nestfun.py
+++ nestfun.py
@@ -1,12 +1,12 @@
 x=int(input("Place a value "))
 y=int(input("Place a second value "))
-#z=int(input("Place a third value "))
+z=int(input("Place a third value "))
 def sum(x, y, z):
     k=x+y+z
     return k
 
 def avg():
-    a=sum(x,y,z=435)
+    a=sum(x,y,z)
     average=a/3
     print(average)
 
